9de828d46bd77b6929fd952a0c96f2c62be3b37b
Abdullah-Mahmoud557 <abdullah.salama@student.giu-uni.de>
2025-08-13 11:52:37 +0300

    Student Managmenet System

---
+++ chapter1/test.js
@@ -0,0 +1,40 @@
+ const students = [] ;
+
+function addStudent(name, score) {
+   if (name === undefined || score === undefined ) console.log("missing info" )  ;
+   else {
+       let studentName = findStudentByName(name);
+          if (studentName !== null ) {
+            console.log("student already exists" ); 
+          }
+          else {  
+             students.push({ name: name, score: score });
+             console.log ("added successfuly")}
+          }
+}
+
+function displayStudents() {
+    if (students.length > 0 )
+       students.forEach(e => console.log (e)); 
+    else console.log("there is no students")
+}
+
+function getAverageScore() {
+    let sum = students.reduce((acc, e) => acc + e.score, 0);
+    let avg = students.length> 0 ? sum/students.length : 0;
+    console.log(avg); 
+}
+function getTopStudent() {
+ let mx = -Infinity ; let top = null ;
+ students.forEach(e=> {
+  if (mx < e.score) {
+    top  = e ;
+    mx = e.score  ; 
+  }
+ })
+ return top === null ? "there is no students" : top; 
+}
+
+function findStudentByName(name) {
+    return students.find(e => e.name.toLowerCase() === name.toLowerCase()) || null;
+}
